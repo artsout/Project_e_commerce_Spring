3cf7cd664a91b8447071a0f623235fab5130c809
Arthur Ferrer <arthurferrerlima@gmail.com>
2026-09-03 15:37:40 -0300

    Initial commit

---
+++ src/main/java/com/e_commerce/Project_E_Commerce_Spring/Application/ProjectECommerceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.Project_E_Commerce_Spring.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectECommerceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectECommerceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_commerce/Project_E_Commerce_Spring/ProjectECommerceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.Project_E_Commerce_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectECommerceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
